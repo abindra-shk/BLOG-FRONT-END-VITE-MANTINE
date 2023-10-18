4f8e993e4b4240bbd3bf89aeb3bb91ceae0f7472
Bijen <gr8bijen@gmail.com>
2023-10-18 18:58:36 +0545

    Feature: Added styling to the login and signup forms

--- src/components/modules/Landing/loginform.tsx
+++ src/components/modules/Landing/loginform.tsx
@@ -1,4 +1,9 @@
 import { DialogBox } from "../../common/DialogBox"
+import email from "../../../assets/emailicon.png"
+import google from "../../../assets/googleicon.png"
+import facebook from "../../../assets/facebookicon.webp"
+import apple from "../../../assets/appleicon.png"
+import twitter from "../../../assets/twittericon.png"
 
 export const LoginForm=(props:any)=>{
     const Signup=()=>{
@@ -8,11 +13,13 @@ export const LoginForm=(props:any)=>{
     }
 return(
     props.open &&(<DialogBox open={props.open} close={props.close} isSwitched={props.isSwitched}>
-        <div>Welcome Back.</div>
-        <div>Sign in with Google</div>
-        <div>Sign in with Facebook</div>
-        <div>Sign in with email</div>
-        <div>No account? <span onClick={Signup} className="text-green-700 font-extrabold cursor-pointer hover:text-green-900">Create one</span></div>
+        <div className="font-serif text-3xl mt-16 mb-16">Welcome back.</div>
+        <div className="border border-black w-1/2 rounded-full my-1 py-1 flex items-center justify-center cursor-pointer"><img className="w-4 h-4 inline ml-2" src={google}/><span className="flex-1 text-center">Sign up with Google</span></div>
+        <div className="border border-black w-1/2 rounded-full my-1 py-1 flex items-center justify-center cursor-pointer"><img className="w-4 h-4 inline ml-2" src={facebook}/><span className="flex-1 text-center">Sign up with Facebook</span></div>
+        <div className="border border-black w-1/2 rounded-full my-1 py-1 flex items-center justify-center cursor-pointer"><img className="w-4 h-4 inline ml-2" src={apple}/><span className="flex-1 text-center">Sign in with Apple</span></div>
+        <div className="border border-black w-1/2 rounded-full my-1 py-1 flex items-center justify-center cursor-pointer"><img className="w-4 h-4 inline ml-2" src={twitter}/><span className="flex-1 text-center">Sign in with Twitter</span></div>
+        <div className="border border-black w-1/2 rounded-full my-1 py-1 flex items-center justify-center cursor-pointer"><img className="w-4 h-4 inline ml-2" src={email}/><span className="flex-1 text-center">Sign up with email</span></div>
+        <div className="mt-8">No account? <span onClick={Signup} className="text-green-600 font-extrabold cursor-pointer hover:text-green-700">Create one</span></div>
     </DialogBox>)
 )
 }
--- src/components/modules/Landing/signupform.tsx
+++ src/components/modules/Landing/signupform.tsx
@@ -1,4 +1,7 @@
 import { DialogBox } from "../../common/DialogBox"
+import email from "../../../assets/emailicon.png"
+import google from "../../../assets/googleicon.png"
+import facebook from "../../../assets/facebookicon.webp"
 
 export const SignupForm=(props:any)=>{
     const SignIn=()=>{
@@ -8,11 +11,11 @@ export const SignupForm=(props:any)=>{
     }
 return(
     props.open &&(<DialogBox open={props.open} close={props.close} isSwitched={props.isSwitched}>
-        <div>Join Medium.</div>
-        <div>Sign up with Google</div>
-        <div>Sign up with Facebook</div>
-        <div>Sign up with email</div>
-        <div>Already have an account? <span onClick={SignIn} className="text-green-700 font-extrabold cursor-pointer hover:text-green-900">Sign in</span></div>
+        <div className="font-serif text-3xl mt-28 mb-16">Join Medium.</div>
+        <div className="border border-black w-1/2 rounded-full my-1 py-1 flex items-center justify-center cursor-pointer"><img className="w-4 h-4 inline ml-2" src={google}/><span className="flex-1 text-center">Sign up with Google</span></div>
+        <div className="border border-black w-1/2 rounded-full my-1 py-1 flex items-center justify-center cursor-pointer"><img className="w-4 h-4 inline ml-2" src={facebook}/><span className="flex-1 text-center">Sign up with Facebook</span></div>
+        <div className="border border-black w-1/2 rounded-full my-1 py-1 flex items-center justify-center cursor-pointer"><img className="w-4 h-4 inline ml-2" src={email}/><span className="flex-1 text-center">Sign up with email</span></div>
+        <div className="mt-8">Already have an account? <span onClick={SignIn} className="text-green-600 font-extrabold cursor-pointer hover:text-green-700">Sign in</span></div>
     </DialogBox>)
 )
 }
